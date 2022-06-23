a047ff848de8a25a390bda8e4114d3edea9efc75
SteBaum <stephan.baum@edu.dsti.institute>
2022-06-24 00:54:02 +0200

    modif shell file

--- install.sh
+++ install.sh
@@ -2,6 +2,4 @@ cd /aws
 wget https://github.com/second-state/OCR-tesseract-on-Centos7/raw/main/deps.tar.gz
 tar -zxvf deps.tar.gz
 
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/aws/deps/"
-
-source ~/.bash_profile
+cp -R /aws/deps/* /aws
