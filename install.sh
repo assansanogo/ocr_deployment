4a95c13d63f4df0603361bebe197450881501aed
SteBaum <stephan.baum@edu.dsti.institute>
2022-06-24 00:46:43 +0200

    modif shell file

--- install.sh
+++ install.sh
@@ -1,37 +1,7 @@
-sudo yum install -y zlib
-sudo yum install -y zlib-devel
-sudo yum install -y libjpeg
-sudo yum install -y libjpeg-devel
-sudo yum install -y libwebp
-sudo yum install -y libwebp-devel
-sudo yum install -y libtiff
-sudo yum install -y libtiff-devel
-sudo yum install -y libpng
-sudo yum install -y libpng-devel
-
-cd /usr/local/lib
-sudo cp /usr/lib64/libjpeg.so.62 .
-sudo cp /usr/lib64/libwebp.so.4 .
-sudo cp /usr/lib64/libtiff.so.5 .
-sudo cp /usr/lib64/libpng15.so.15 .
-
-mkdir /aws
 cd /aws
-git clone https://github.com/DanBloomberg/leptonica.git --depth 1
-cd /aws/leptonica
-./autogen.sh
-./configure --prefix=/usr/local --disable-shared --enable-static --with-zlib --with-jpeg --with-libwebp  --with-libtiff --with-libpng
-make
-sudo make install
-sudo ldconfig
+wget https://github.com/second-state/OCR-tesseract-on-Centos7/raw/main/deps.tar.gz
+tar -zxvf deps.tar.gz
 
-cd /aws
-wget https://github.com/tesseract-ocr/tesseract/archive/4.0.0.tar.gz -O tesseract-4.0.0.tar.gz
-tar zxvf tesseract-4.0.0.tar.gz
-cd tesseract-4.0.0
-export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
-./autogen.sh
-./configure --prefix=/usr/local --disable-shared --enable-static --with-extra-libraries=/usr/local/lib/ --with-extra-includes=/usr/local/lib/
-make
-sudo make install
-sudo ldconfig
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/aws/deps/"
+
+source ~/.bash_profile
